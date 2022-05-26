043918e368190c4a86a4a0306a88564f6b3916fb
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-05-26 08:35:12 -0500

    [wip] test for `FileSystemDatastore.new([dir])`

--- tests/datastore/test_filesystem_datastore.nim
+++ tests/datastore/test_filesystem_datastore.nim
@@ -1,3 +1,5 @@
+import std/os
+
 import pkg/questionable
 import pkg/questionable/results
 from pkg/stew/results as stewResults import get, isErr, isOk
@@ -6,7 +8,20 @@ import pkg/unittest2
 import ../datastore/filesystem_datastore
 
 suite "FileSystemDatastore":
-    test "first light":
+  setup:
+    # assumes tests/test_all is run from project root, e.g. with `nimble test`
+    let
+      root = "tests" / "data"
 
-        check:
-          1 == 1
+    removeDir(getCurrentDir() / root)
+
+  teardown:
+    removeDir(getCurrentDir() / root)
+
+  test "new":
+    let
+      ds = FileSystemDatastore.new("tests" / "data")
+
+    check:
+      ds.isOk
+      dirExists(getCurrentDir() / root)
