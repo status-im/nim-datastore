94b5e2bf0a17a7c5b9cf6c6631451e8460b67ce2
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-06-01 20:13:55 -0500

    [wip] `new` test for SQLiteDatastore

--- tests/datastore/test_sqlite_datastore.nim
+++ tests/datastore/test_sqlite_datastore.nim
@@ -10,21 +10,96 @@ import ../../datastore/sqlite_datastore
 
 suite "SQLiteDatastore":
   setup:
+    var
+      db: SQLiteDatastore
+
     # assumes tests/test_all is run from project root, e.g. with `nimble test`
     let
-      dbPath = "tests" / "data" / "store" & dbExt
-      dbPathAbs = getCurrentDir() / dbPath
+      basePath = "tests" / "test_data"
+      basePathAbs = getCurrentDir() / basePath
+      filename = "test_store" & dbExt
+      dbPathAbs = basePathAbs / filename
 
+    db = nil
     removeDir(dbPathAbs)
-    assert not dirExists(dbPathAbs)
+    assert not dirExists(basePathAbs)
 
   teardown:
-    removeDir(dbPathAbs)
-    assert not dirExists(dbPathAbs)
+    if not db.isNil:
+      db.close
+      db = nil
+
+    removeDir(basePathAbs)
+    assert not dirExists(basePathAbs)
 
   test "new":
+    var
+      dbRes: Result[SQLiteDatastore, ref CatchableError]
+
+    dbRes = SQLiteDatastore.new(basePathAbs, filename)
+
+    assert dbRes.isOk
+    db = dbRes.get
+
     check:
-      true
+      dirExists(basePathAbs)
+      fileExists(dbPathAbs)
+
+    db.close
+    db = nil
+    removeDir(basePathAbs)
+    assert not dirExists(basePathAbs)
+
+    dbRes = SQLiteDatastore.new(basePath, filename)
+
+    assert dbRes.isOk
+    db = dbRes.get
+
+    check:
+      dirExists(basePathAbs)
+      fileExists(dbPathAbs)
+
+    db.close
+    db = nil
+
+    # for `readOnly = true` to succeed the database file must already exist on
+    # disk, so the existing file (per previous step) is not deleted prior to
+    # the next invocation of SQLiteDatastore.new
+
+    dbRes = SQLiteDatastore.new(basePath, filename, readOnly = true)
+
+    assert dbRes.isOk
+    db = dbRes.get
+
+    check:
+      dirExists(basePathAbs)
+      fileExists(dbPathAbs)
+
+    db.close
+    db = nil
+    removeDir(basePathAbs)
+    assert not dirExists(basePathAbs)
+
+    dbRes = SQLiteDatastore.new(inMemory = true)
+
+    assert dbRes.isOk
+    db = dbRes.get
+
+    check:
+      not dirExists(basePathAbs)
+      not fileExists(dbPathAbs)
+
+    ds.close
+    ds = nil
+
+    dbRes = SQLiteDatastore.new(readOnly = true, inMemory = true)
+
+    assert dbRes.isOk
+    db = dbRes.get
+
+    check:
+      not dirExists(basePathAbs)
+      not fileExists(dbPathAbs)
 
   test "accessors":
     check:
