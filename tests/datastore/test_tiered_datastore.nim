40955e19f849f6be864419e9459ddaae98522ae8
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-06-09 00:04:14 -0500

    [wip] tests for TieredDatastore

--- tests/datastore/test_tiered_datastore.nim
+++ tests/datastore/test_tiered_datastore.nim
@@ -10,34 +10,143 @@ import ../../datastore/tiered_datastore
 
 suite "TieredDatastore":
   setup:
-    discard
+    # assumes tests/test_all is run from project root, e.g. with `nimble test`
+    let
+      bytes = @[1.byte, 2.byte, 3.byte]
+      key = Key.init("a:b/c/d:e").get
+      root = "tests" / "test_data"
+      rootAbs = getCurrentDir() / root
+
+    discard bytes # suppresses "declared but not used" re: bytes
+    discard key # # suppresses "declared but not used" re: key
+
+    removeDir(rootAbs)
+    require(not dirExists(rootAbs))
+
+    var
+      ds1 = SQLiteDatastore.new(inMemory = true).get
+      ds2 = FileSystemDatastore.new(rootAbs).get
+
+    discard ds2 # suppresses "declared but not used" re: ds2
 
   teardown:
-    discard
+    ds1.close
+    removeDir(rootAbs)
+    require(not dirExists(rootAbs))
 
   test "new":
     check:
-      true
+      TieredDatastore.new().isErr
+      TieredDatastore.new([]).isErr
+      TieredDatastore.new(@[]).isErr
+      TieredDatastore.new(ds1, ds2).isOk
+      TieredDatastore.new([ds1, ds2]).isOk
+      TieredDatastore.new(@[ds1, ds2]).isOk
 
   test "accessors":
+    let
+      stores = @[ds1, ds2]
+
     check:
-      true
+      TieredDatastore.new(ds1, ds2).get.stores == stores
+      TieredDatastore.new([ds1, ds2]).get.stores == stores
+      TieredDatastore.new(@[ds1, ds2]).get.stores == stores
 
   test "put":
+    let
+      ds = TieredDatastore.new(ds1, ds2).get
+
+    assert ds1.get(key).get.isNone
+    assert ds2.get(key).get.isNone
+
+    let
+      putRes = ds.put(key, bytes)
+
     check:
-      true
+      putRes.isOk
+      ds1.get(key).get.get == bytes
+      ds2.get(key).get.get == bytes
 
   test "delete":
+    let
+      ds = TieredDatastore.new(ds1, ds2).get
+      putRes = ds.put(key, bytes)
+
+    assert putRes.isOk
+    assert ds1.get(key).get.get == bytes
+    assert ds2.get(key).get.get == bytes
+
+    let
+      delRes = ds.delete(key)
+
     check:
-      true
+      delRes.isOk
+      ds1.get(key).get.isNone
+      ds2.get(key).get.isNone
 
   test "contains":
+    let
+      ds = TieredDatastore.new(ds1, ds2).get
+
+    assert not ds1.contains(key).get
+    assert not ds2.contains(key).get
+
+    let
+      putRes = ds.put(key, bytes)
+
+    assert putRes.isOk
+
+    let
+      containsRes = ds.contains(key)
+
     check:
-      true
+      containsRes.isOk
+      containsRes.get
+      ds1.contains(key).get
+      ds2.contains(key).get
 
   test "get":
+    var
+      ds = TieredDatastore.new(ds1, ds2).get
+
+    assert ds1.get(key).get.isNone
+    assert ds2.get(key).get.isNone
+
+    check: ds.get(key).get.isNone
+
+    let
+      putRes = ds.put(key, bytes)
+
+    assert putRes.isOk
+
+    var
+      getRes = ds.get(key)
+
     check:
-      true
+      getRes.isOk
+      getRes.get.isSome
+      getRes.get.get == bytes
+      ds1.get(key).get.isSome
+      ds2.get(key).get.isSome
+      ds1.get(key).get.get == bytes
+      ds2.get(key).get.get == bytes
+
+    ds1.close
+    ds1 = SQLiteDatastore.new(inMemory = true).get
+    ds = TieredDatastore.new(ds1, ds2).get
+
+    assert ds1.get(key).get.isNone
+    assert ds2.get(key).get.isSome
+    assert ds2.get(key).get.get == bytes
+
+    getRes = ds.get(key)
+
+    check:
+      getRes.isOk
+      getRes.get.isSome
+      getRes.get.get == bytes
+      ds1.get(key).get.isSome
+      ds1.get(key).get.get == bytes
 
   # test "query":
   #   check:
